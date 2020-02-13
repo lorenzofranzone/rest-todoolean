115bda7f55b0f8e97e82daa5fe4990d6244da703
Lorenzo <lorenzo.franzone@gmail.com>
2020-02-13 19:37:40 +0100

    Delete Click

--- scripts.js
+++ scripts.js
@@ -1,22 +1,32 @@
 $(document).ready(function(){
 
   // Code
-  // Click on button
-  // $(document).on('click', 'button', function(){
-  //
-  // }) // Click Button
-  //
-  // // Keypress on input
-  // $('input').keypress(function(event){
-  //   var userNewTodo = $('input').val();
-  //   if (event.which == 13) {
-  //
-  //   }
-  // });
-
   //  http://157.230.17.132:3017/todos
 
+  // Get todo list
   getTodosList();
+
+  // Click button
+  $('.add-todo-container button').click(function(){
+    // Get input value
+    var todoValue = $('.add-todo-container input').val();
+    addNewTodo(todoValue);
+  }); // Click button
+
+  // Keypress input
+  $('.add-todo-container input').keypress(function(event){
+    var todoValue = $('.add-todo-container input').val();
+    if (event.which == 13) {
+      addNewTodo(todoValue);
+    }
+  }); // Keypress input
+
+  // Delete click
+  $(document).on('click', 'i.delete-todo', function() {
+    var iconDelete = $(this);
+    var idToDelete = iconDelete.parent().attr('data-id');
+    deleteTodo(idToDelete);
+  }); // Delete click
 
 
 
@@ -29,7 +39,7 @@ $(document).ready(function(){
     $.ajax ({
       url: 'http://157.230.17.132:3017/todos',
       method: 'GET',
-      success : function(data) {
+      success: function(data) {
         // Get API content
         var listTodos = data;
 
@@ -43,17 +53,56 @@ $(document).ready(function(){
           var todo = listTodos[i];
           // Handlebars injection
           var context = {
-            text: todo.text
+            text: todo.text,
+            id: todo.id
           }
           var html = template(context);
+          var todoValue = $('.add-todo-container input').val('');
           $('.list-todos').append(html);
         } // Loop
       }, // success
-      error : function(request, state, errors) {
+      error: function(request, state, errors) {
         alert('errors');
       } // error
     }); // ajax
-  }
+  } // FX getTodosList
+
+
+
+  // FX ADD NEW TODO
+  function addNewTodo(todoValue) {
+    $.ajax ({
+      url: 'http://157.230.17.132:3017/todos',
+      method: 'POST',
+      data: {
+        text: todoValue
+      },
+      success: function(data) {
+        $('.list-todos').html('');
+        getTodosList();
+      }, // success
+      error: function(request, state, errors) {
+        alert('errors');
+      } // error
+    }); // ajax
+  } // FX addNewTodo
+
+
+
+  // FX DELETE TODO
+  function deleteTodo(id) {
+    $.ajax ({
+      url: 'http://157.230.17.132:3017/todos/' + id,
+      method: 'DELETE',
+      success: function(data) {
+        $('.list-todos').html('');
+        getTodosList();
+      }, // success
+      error: function(request, state, errors) {
+        alert('errors');
+      } // error
+    }); // ajax
+  } // FX addNewTodo
 
 
 //////////
