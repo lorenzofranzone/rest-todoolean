48cd99d418b0be5774a13227c00bd0fa97e814b4
Lorenzo <lorenzo.franzone@gmail.com>
2020-02-13 16:52:18 +0100

    Start

---
+++ scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+  // Code
+  // Click on button
+  // $(document).on('click', 'button', function(){
+  //
+  // }) // Click Button
+  //
+  // // Keypress on input
+  // $('input').keypress(function(event){
+  //   var userNewTodo = $('input').val();
+  //   if (event.which == 13) {
+  //
+  //   }
+  // });
+
+  //  http://157.230.17.132:3017/todos
+
+
+  //////////////////////////////////////////////////
+  // F U N C T I O N S
+  //////////////////////////////////////////////////
+
+
+
+
+
+//////////
+});
